7c3add9aa5bc8e79f89b35266ed991e44d2a79fc
Cristian Contreras <contreras.cristian994@gmail.com>
2023-06-04 09:35:22 -0400

    set up img2

--- src/data.jsx
+++ src/data.jsx
@@ -22,18 +22,18 @@ export const projects = [
         id: 1,
         url: 'https://letsdothis94.github.io/encriptador/',
         title: 'Encriptador de Palabras Alura',
-        image: './src/images/encriptadorReadme.png',
+        image: 'images/encriptadorReadme.png',
     },
     {
         id: 2,
         url: 'https://github.com/Letsdothis94/gtApp',
         title: 'GT-App, "Getting people together"',
-        image: '/src/images/gt_app.png',
+        image: 'images/gt_app.png',
     },
     {
         id: 3,
         url: 'https://github.com/Letsdothis94/boringApp',
         title: 'The Boring App',
-        image: 'src/images/chat.png',
+        image: 'images/chat.png',
     },
 ];
