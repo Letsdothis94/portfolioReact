f82b529a75b4bd84c675766555835670946a4e55
Cristian Contreras <contreras.cristian994@gmail.com>
2024-03-14 17:23:19 -0400

    [about]: updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from './components/NavBar/Navbar';
 import Home from './components/Home';
-import About from './components/About';
+import About from './components/About/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
